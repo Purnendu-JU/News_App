21d3385cf857da79de9e3754e60ce103ec671917
Purnendu-JU <purnendukumarmisra9@gmail.com>
2024-06-26 15:21:46 +0530

    Ready to Deploy

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to Mongo");
+}
+main();
+const app = express();
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
